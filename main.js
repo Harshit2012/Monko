79f845b47aaf88228731b14cb5ca6b41161c0624
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2022-05-08 12:25:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function Find_out(){
+    window.location = "screen2.html"
+    alert("loading...")
+}
---
+++ main2.js
@@ -0,0 +1,12 @@
+function open1(){
+    window.location = "https://learnprogramming.academy"
+    alert("loading...")
+}
+function open2(){
+    window.location = "https://www.whitehatjr.com"
+    alert("loading...")
+}
+function open3(){
+    window.location = "https://courses.pieriandata.com/collections"
+    alert("loading...")
+}
