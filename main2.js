7f6672773954d9bb2b81ab3bc63636c516e65353
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2022-05-22 11:45:07 +0530

    Update main2.js
    Add console code

--- main2.js
+++ main2.js
@@ -1,12 +1,15 @@
 function open1(){
     window.location = "https://learnprogramming.academy"
     alert("loading...")
+    console.log("loading...")
 }
 function open2(){
     window.location = "https://www.whitehatjr.com"
     alert("loading...")
+    console.log("loading...")
 }
 function open3(){
     window.location = "https://courses.pieriandata.com/collections"
-    alert("loading...")
+    alert("loading...") 
+    console.log("loading...")
 }
